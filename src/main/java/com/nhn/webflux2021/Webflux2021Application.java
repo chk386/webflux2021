ddf3dd17ac3585eb4dde76c4195f23e43c251540
haekyu.cho <haekyu.cho@nhn.com>
2021-10-31 02:40:05 +0900

    add reactive-streams testcase

--- src/main/java/com/nhn/webflux2021/Webflux2021Application.java
+++ src/main/java/com/nhn/webflux2021/Webflux2021Application.java
@@ -1,14 +1,21 @@
 package com.nhn.webflux2021;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Flow;
+import java.util.concurrent.FutureTask;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -16,15 +23,14 @@ public class Webflux2021Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Webflux2021Application.class, args);
+
+        Flux.
     }
 
-
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+    ApplicationRunner run(ApplicationContext context) {
         return args -> {
-            System.out.println("args" + String.join(",", args));
-
 
         };
     }
- }
+}
---
+++ src/test/java/com/nhn/webflux2021/ReactiveStreamTest.java
@@ -0,0 +1,123 @@
+package com.nhn.webflux2021;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Observable;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.in;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(OutputCaptureExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ReactiveStreamTest {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final List<Integer> integers = List.of(1, 2, 3, 4, 5);
+
+    @Test
+    @Order(1)
+    @DisplayName("Iterable 테스트")
+    void iterableTest(CapturedOutput output) {
+        for (Iterator<Integer> it = integers.iterator(); it.hasNext(); ) {
+            Integer integer = it.next();
+
+            logger.info("Iterable Pattern : {}", integer);
+        }
+
+        assertThat("1,2,3,4,5가 출력되어야 한다.", captureOutput(output), everyItem(is(in(integers))));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Observable 테스트")
+    void observableTest(CapturedOutput output) {
+        ExamObservable observable = new ExamObservable();
+        observable.addObserver((o, arg) -> logger.info("Observable Pattern : {}", arg));
+
+        observable.push(integers);
+
+        assertThat("1,2,3,4,5가 출력되어야 한다.", captureOutput(output), everyItem(is(in(integers))));
+    }
+
+    static class ExamObservable extends Observable {
+        void push(List<Integer> integers) {
+            integers.forEach(i -> {
+                this.setChanged();
+                this.notifyObservers(i);
+            });
+        }
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Reactive Streams 테스트")
+    void reactiveStreamsTest(CapturedOutput output) {
+        Publisher<Integer> publisher = s -> integers.forEach(s::onNext);
+        publisher.subscribe(new Subscriber<>() {
+            private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+            @Override
+            public void onSubscribe(Subscription s) {
+                logger.debug("onSubscribe");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                // publisher가 데이터를 push할때 실행
+                logger.info("Reactive Streams : {}", integer);
+
+                // 시스템 로드 평균이 90을 넘을경우
+//            if(ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class).getSystemLoadAverage() > 90) {
+//                subscription.request(3);
+//            }else {
+//                subscription.request(1);
+//            }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                logger.error(t.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                logger.debug("onComplete");
+            }
+        });
+
+        assertThat("1,2,3,4,5가 출력되어야 한다.", captureOutput(output), everyItem(is(in(integers))));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Reactor 테스트")
+    void reactorTest(CapturedOutput output) {
+        Flux.fromIterable(integers)
+                .subscribe(v -> logger.info("Reactor : {}", v));
+
+        assertThat("1,2,3,4,5가 출력되어야 한다.", captureOutput(output), everyItem(is(in(integers))));
+    }
+
+    private List<Integer> captureOutput(CapturedOutput output) {
+        return Arrays.stream(output.getOut()
+                        .split("\n"))
+                .filter(line -> line.contains("DualityTest"))
+                .map(line -> Integer.parseInt(line.substring(line.length() - 1)))
+                .collect(toList());
+    }
+}
