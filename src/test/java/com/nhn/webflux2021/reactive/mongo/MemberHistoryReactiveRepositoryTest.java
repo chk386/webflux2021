5e8055a3d3ddc615c620a3f1d07c736ca59c96a8
haekyu.cho <haekyu.cho@nhn.com>
2021-11-05 02:07:01 +0900

    add reactive mongo tests

--- src/test/java/com/nhn/webflux2021/reactive/mongo/MemberHistoryReactiveRepositoryTest.java
+++ src/test/java/com/nhn/webflux2021/reactive/mongo/MemberHistoryReactiveRepositoryTest.java
@@ -2,30 +2,44 @@ package com.nhn.webflux2021.reactive.mongo;
 
 import com.nhn.webflux2021.configuration.MongoConfig;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ContextConfiguration;
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 @ContextConfiguration(classes = MongoConfig.class)
 class MemberHistoryReactiveRepositoryTest {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     MemberHistoryReactiveRepository memberRepository;
 
     @Test
     void createHistoryTest() {
+        Flux.range(1, 1000)
+            .map(MemberHistoryReactiveRepositoryTest::of)
+            .doOnNext(memberHistory -> System.out.println(memberHistory.toString()))
+            .as(memberRepository::saveAll)
+            .doOnNext(memberHistory -> log.debug("result : {}", memberHistory.toString()))
+            .as(StepVerifier::create)
+            .expectNextCount(1000)
+            .verifyComplete();
+    }
+
+    private static MemberHistory of(Integer id) {
         MemberHistory memberHistory = new MemberHistory();
-        memberHistory.setMemberId(1);
+        memberHistory.setMemberId(id);
         memberHistory.setCreatedAt(LocalDateTime.now());
 
-        memberRepository.save(memberHistory)
-                .doOnNext(v -> System.out.println(v.toString()))
-                .as(StepVerifier::create)
-                .expectNextCount(1)
-                .verifyComplete();
+        return memberHistory;
     }
 }
